42001bf10cd68ff84bbd09c6a6f21542abcbf3cb
cecilia ortez <est.cortec0201@uml.edu.ni>
2023-04-15 12:37:44 -0600

    Creacion de la pagina de detalles y rectofactorizacion de detalles

--- main.js
+++
@@ -1,5 +0,0 @@
-//cargar los estilos de la aplicacion 
-import './style.css'
-
-//activar el plugin de flowbite
-import 'flowbite'
---
+++ src/js/main.js
@@ -0,0 +1,14 @@
+//cargar los estilos de la aplicacion 
+import '../css/style.css'
+
+//activar el plugin de flowbite
+import 'flowbite'
+
+//cargar la barra de navegaciòn
+import './barra'
+
+//cargar el menu del carusel/slider
+import './slider'
+
+//cargar el encabezado del header
+import './header'
